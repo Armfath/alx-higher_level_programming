65a4b44af1ef6e62188de6443f8b115f94c1307b
ANAGONOU Armand <info.armanagonou.com>
2022-11-30 04:04:05 +0100

    fizz buzz

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,7 +10,7 @@ def fizzbuzz():
         elif ((i % 3) == 0):
             print("Fizz{}".format(str), end='')
         elif ((i % 5) == 0):
-            print("Buzz", end='')
+            print("Buzz{}".format(str), end='')
         else:
             print("{}{}".format(i, str), end='')
     print("")
