17a3af4c15bfac79393bccf030752dbbaf897c69
ANAGONOU Armand <info.armanagonou.com>
2022-11-30 16:07:10 +0100

    add node in shorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,6 +19,12 @@ listint_t *insert_node(listint_t **head, int number)
 		return (NULL);
 	}
 	new->n = number;
+	if ((*head) == NULL)
+	{
+		(*head) = new;
+		return (*head);
+	}
+	
 	if (number <= current->n)
 	{
 		new->next = current;
