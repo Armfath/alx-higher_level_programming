a869f10ad26761f026eb33f6741002bf66c460f4
ANAGONOU Armand <info.armanagonou.com>
2022-12-01 17:07:08 +0100

    fix in args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif (len == 2):
         print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(len -1))
+        print("{} arguments:".format(len - 1))
         for i in range(1, len):
             print("{}: {}".format(i, sys.argv[i]))
