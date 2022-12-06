8a54e0d5c6028a7fed9bcfc4342b62a0e872f9ad
ANAGONOU Armand <info.armanagonou.com>
2022-12-06 03:22:45 +0100

    add done

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 def print_list_integer(my_list=[]):
     length = len(my_list)
-    for i in range(0, lenght):
-        print("{}".format(my_list[i]))
+    for i in range(0, length):
+        print("{:d}".format(my_list[i]))
