12b30c65b8e83c1a0cb89f678d124c5fe82cd588
ANAGONOU Armand <info.armanagonou.com>
2022-12-06 03:43:36 +0100

    add done fix 4

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,9 @@
 def no_c(my_string):
-    
+    """
+    removes all characters 'c' and 'C' from s
+    """
+    new_str = ""
+    for i in my_string:
+        if i not in "cC":
+            new_str += i
+    return (new_str)
