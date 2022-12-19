41428947fba1d8b4210ec20581e2d16f0c8c4966
ANAGONOU Armand <info.armanagonou.com>
2022-12-19 10:24:51 +0100

    remove all C and c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
+#!/usr/bin/python3
+
 def no_c(my_string):
-    """
-    removes all characters 'c' and 'C' from s
-    """
-    new_str = ""
-    for i in my_string:
-        if i not in "cC":
-            new_str += i
-    return (new_str)
+	string = ""
+	for c in my_string:
+		if (ord(c) != ord("c") and ord(c) != ord("C")):
+			string += c
+	return (string)
