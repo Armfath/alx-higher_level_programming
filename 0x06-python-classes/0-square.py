4dea70e05247c544d56ce0172d4fc8a562fe4780
ANAGONOU Armand <info.armanagonou.com>
2022-12-20 17:47:06 +0100

    create a empty class + docmentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
 This module contain classes definition
 """
 class Square:
     """
     This is an Empty class that defines a square
+    
     Attributes:
         empty
     """
