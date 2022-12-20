a904a79bd0a375581f91fae2b22a1af3b25f6c70
ANAGONOU Armand <info.armanagonou.com>
2022-12-20 18:56:04 +0100

    retuurn value from instance mthod + docstring 0-0

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,35 +1,24 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
 """
-Created on Tue May 19 14:21:54 2020
-@author: Robinson Montes
+This module contain classes definition
 """
 
 
 class Square:
-    """Class Square that has attributes. Instantiation with size
-    Attributes:
-        size (int): The size of the square
     """
-
+    Define a Square
+    """
     def __init__(self, size=0):
-        """The __init__ method for Square class
-        Args:
-            size: (:obj: 'int', optional): A private instance size
-        Raises:
-            TypeError: Exception if size is not an integer
-            ValueError: Exception if size is less than 0
-        """
-        if type(size) is not int:
+        self.__size = size
+        if type(self.__size) != int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if (self.__size < 0):
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
-
     def area(self):
-        """Calculates the area of the square
-        Returns:
-            The square area
         """
-        return self.__size ** 2
+        It return the area of the square
+
+        Returns:
+            - int: The area of the square
+        """
+        return (self.__size * self.__size)
