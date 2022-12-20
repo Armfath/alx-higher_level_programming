a80df04dff7c809d433d3901c3bf1acb79b38c9a
ANAGONOU Armand <info.armanagonou.com>
2022-12-20 18:04:52 +0100

    add private instanceattribute to my class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contain classes definition
+"""
+
+
+class Square:
+    """
+    Define a Square
+    """
+    def __init__(self, size):
+        self.__size = size
