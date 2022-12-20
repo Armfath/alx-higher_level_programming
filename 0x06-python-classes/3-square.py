6f937743fb23166de8dab25e4603f02d690eb0b1
ANAGONOU Armand <info.armanagonou.com>
2022-12-20 18:36:17 +0100

    retuurn value from instance mthod

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contain classes definition
+"""
+
+
+class Square:
+    """
+    Define a Square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        if (self.__size < 0):
+            raise ValueError("size must be >= 0")
+    def area(self):
+        return (self.__size * self.__size)
