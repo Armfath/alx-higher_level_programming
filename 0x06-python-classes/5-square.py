20e4c2ea36e20f0e65f055d7baea80a7a1199a07
Armfath <info.armanangou@gmail.com>
2022-12-21 10:31:34 +0100

    add method that print the square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,6 +15,26 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    @property
+    def size(self):
+        """
+        size getter
+        """
+
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """
+        size setter
+        """
+
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
     def area(self):
         """
         It return the area of the square
@@ -24,3 +44,15 @@ class Square:
         """
 
         return (self.__size ** 2)
+
+    def my_print(self):
+        """
+        prints in stdout the square with the character #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
