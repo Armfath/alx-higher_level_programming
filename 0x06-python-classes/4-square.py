eb9f3c422829bfabd6ce89ac5c993f4208c06f72
Armfath <info.armanangou@gmail.com>
2022-12-21 10:20:40 +0100

    setter and getter added

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,17 +14,21 @@ class Square:
         if (size < 0):
             raise ValueError("size must be >= 0")
         self.__size = size
-    
+
+    @property
     def size(self):
         """
         size getter
         """
-        return self.__size
-    
+
+        return (self.__size)
+
+    @size.setter
     def size(self, value):
         """
         size setter
         """
+
         if type(value) != int:
             raise TypeError("size must be an integer")
         if (value < 0):
