8a2c30199d08c1a28306b8bf5ad53ce6a30b2162
Armfath <info.armanangou@gmail.com>
2023-01-09 21:52:04 +0100

    add method to draw

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -22,6 +22,8 @@ class Rectangle:
 
     def __str__(self):
         rectangle = ""
+        if (self.__width > 0 or self.__height > 0)
+            return (rectangle)
         for i in range(self.__height):
             for j in range(self.__width):
                 rectangle += "#"
