fec9b34ea0d5b240c05649bda2b20ce25d3dc7c7
Armfath <info.armanangou@gmail.com>
2023-01-11 16:20:09 +0100

    write in a file with orverwrite if it already exists

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contain file oparations
+"""
+
+
+def read_file(filename="", text=""):
+    """
+    Function that write to a file and returns number of charater written
+    """
+
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        return a_file.write(text)
