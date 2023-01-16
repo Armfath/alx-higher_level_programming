99af88fee013ef75cc25ee9d7f3eb011763c4ebb
Armfath <info.armanangou@gmail.com>
2023-01-16 16:06:09 +0100

    first test written

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,7 +15,7 @@ def add_integer(a, b=98):
             a = int(a)
         else:
             raise TypeError("a must be an integer")
-    if type(b) is int:
+    if type(b) is not int:
         if type(b) is float and b == b and abs(b) <= 1.7976931348623158e+308:
             b = int(b)
         else:
