b5bd6cb07dcead71656b3230d1a93b622108483b
Armfath <info.armanangou@gmail.com>
2023-01-17 12:44:36 +0100

    start myslef

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,14 +10,13 @@ def add_integer(a, b=98):
     """
     adds two integers
     """
-    if type(a) is not int:
-        if type(a) is float and a == a and abs(a) <= 1.7976931348623158e+308:
-            a = int(a)
-        else:
-            raise TypeError("a must be an integer")
-    if type(b) is not int:
-        if type(b) is float and b == b and abs(b) <= 1.7976931348623158e+308:
-            b = int(b)
-        else:
-            raise TypeError("b must be an integer")
+    if not(isinstance(a, (int, float))):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+            
+    if not(isinstance(b, (int, float))):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
     return a + b
