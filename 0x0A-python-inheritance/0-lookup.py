1863916cbfff2885faac970734357c1d09e85be7
Armfath <info.armanangou@gmail.com>
2023-01-18 14:51:38 +0100

    display all variable of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -8,8 +8,5 @@ def lookup(obj):
     """
     returns the list of available attributes and methods of an object
     """
-    list = []
-    for var in obj.__dict__:
-        list.append(var)
 
-    return list
+    return dir(obj)
