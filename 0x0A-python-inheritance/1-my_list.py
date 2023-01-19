8660cafb419defde928ecef484ace5ee7a7e6541
Armfath <info.armanangou@gmail.com>
2023-01-19 12:28:20 +0100

    Inheritance and test for an instance of class

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+#!usr/bin/python3
+"""
+Inherited class and perfom tests
+"""
+
+
+class MyList(list):
+    """
+    Mylist that inherits from list class
+    """
+
+    def print_sorted(self):
+        """
+        sort the list (asc)
+        """
+
+        sorted_list = self[:]
+        sorted_list.sort()
+        print(sorted_list)
