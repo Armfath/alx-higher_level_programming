9e4a0c1ffc91a937c2999dda1dcd5165dc2a5f1d
Armfath <info.armanangou@gmail.com>
2023-01-19 15:41:39 +0100

    raises exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function module
+"""
+
+
+class BaseGeometry():
+    """
+    Empty class raises error Exception
+    """
+    def area(self):
+        """
+        Raises exception
+        """
+        
+        raise Exception("area() is not implemented")
