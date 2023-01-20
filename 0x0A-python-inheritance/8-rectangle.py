76decd005deca5ca91e6b4f804b387efff3beb3a
Armfath <info.armanangou@gmail.com>
2023-01-20 15:38:32 +0100

    attributes validated using the parent method

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""
+Class rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle implamatation
+    """
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
