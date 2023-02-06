ddbb86fc47b93a146a05c365d00388a675e66154
Armfath <info.armanangou@gmail.com>
2023-02-06 09:40:16 +0100

    jjj

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""
-Tests for base class
-"""
-
-
 import unittest
 from models.base import Base
+import json
+import inspect
+
+'''
+    Creating test cases for the base module
+'''
 
 
-class TestBase(unittest.TestCase):
-    """
-    Base tests
-    """
+class test_base(unittest.TestCase):
+    '''
+        Testing base
+    '''
+    def test_id_none(self):
+        '''
+            Sending no id
+        '''
+        b = Base()
+        self.assertEqual(1, b.id)
 
-    def test_basics(self):
-        """
-        Tests basics values
-        """
-        b1 = Base()
-        self.assertEqual(b1.id, 1)
-        b2 = Base()
-        self.assertEqual(b2.id, 2)
-        b3 = Base(12)
-        self.assertEqual(b3.id, 12)
-        b4 = Base()
-        self.assertEqual(b4.id, 3)
-if __name__ == '__main__':
-    unittest.main()
+    def test_id(self):
+        '''
+            Sending a valid id
+        '''
+        b = Base(50)
+        self.assertEqual(50, b.id)
+
+    def test_id_zero(self):
+        '''
+            Sending an id 0
+        '''
+        b = Base(0)
+        self.assertEqual(0, b.id)
