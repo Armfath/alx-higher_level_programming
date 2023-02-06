2a33821b7ae9e10ee82024a1b4667128fabe2c55
Armfath <info.armanangou@gmail.com>
2023-02-06 08:48:45 +0100

    rarara

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,18 +5,19 @@ Base Module
 
 
 class Base():
-	""""
-	Class Base
-	"""
+    """"
+    Class Base
+    """
 
-	__nb_objects = 0
-	def __init__(self, id=None):
-		"""
-		Initialisation
-		"""
+    __nb_objects = 0
 
-		if id is None:
-			Base.__nb_objects += 1
-			self.id = Base.__nb_objects
-		else:
-			self.id = id
+    def __init__(self, id=None):
+        """
+        Initialisation
+        """
+
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
