9007823499f5302a4edac12d5766e2ff7c6a4056
Armfath <info.armanagonou@gmail.com>
2023-02-09 06:59:32 +0100

    Validations

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,24 +11,58 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Validate and Initiate
+        """
         super().__init__(id)
+        self._validate_integer_input(width, "width")
+        self._validate_positive_integer_input(width, "width")
         self.__width = width
+        self._validate_integer_input(height, "height")
+        self._validate_positive_integer_input(height, "height")
         self.__height = height
+        self._validate_integer_input(x, "x")
+        self._validate_non_negative_integer_input(x, "x")
         self.__x = x
+        self._validate_integer_input(y, "y")
+        self._validate_non_negative_integer_input(y, "y")
         self.__y = y
+
+    def _validate_integer_input(self, value, attr_name):
+        """
+        Is integer
+        """
+        if type(value) != int:
+            raise TypeError(f"{attr_name} must be an integer")
+
+    def _validate_positive_integer_input(self, value, attr_name):
+        """
+        Is positive (> 0)
+        """
+        if value <= 0:
+            raise ValueError(f"{attr_name} must be > 0")
+
+    def _validate_non_negative_integer_input(self, value, attr_name):
+        """
+        Is no-negative (>= 0)
+        """
+        if value < 0:
+            raise ValueError(f"{attr_name} must be >= 0")
 
     @property
     def width(self):
         """
         width getter
         """
-        return (self.__width)
+        return self.__width
 
     @width.setter
     def width(self, value):
         """
         width setter
         """
+        self._validate_integer_input(value, "width")
+        self._validate_positive_integer_input(value, "width")
         self.__width = value
 
     @property
@@ -36,13 +70,15 @@ class Rectangle(Base):
         """
         height getter
         """
-        return (self.__height)
+        return self.__height
 
     @height.setter
     def height(self, value):
         """
-        width setter
+        height setter
         """
+        self._validate_integer_input(value, "height")
+        self._validate_positive_integer_input(value, "height")
         self.__height = value
 
     @property
@@ -50,13 +86,15 @@ class Rectangle(Base):
         """
         x getter
         """
-        return (self.__x)
+        return self.__x
 
     @x.setter
     def x(self, value):
         """
         x setter
         """
+        self._validate_integer_input(value, "x")
+        self._validate_non_negative_integer_input(value, "x")
         self.__x = value
 
     @property
@@ -64,11 +102,13 @@ class Rectangle(Base):
         """
         y getter
         """
-        return (self.__y)
+        return self.__y
 
     @y.setter
     def y(self, value):
         """
         y setter
         """
+        self._validate_integer_input(value, "y")
+        self._validate_non_negative_integer_input(value, "y")
         self.__y = value
