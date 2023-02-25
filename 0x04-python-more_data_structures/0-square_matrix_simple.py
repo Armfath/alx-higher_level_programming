184ad50e591d71d62fedc7a8c3fb5129dc250e0f
Armfath <info.armanagonou@gmail.com>
2023-02-25 14:54:44 +0100

    more data structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 
-class P:
-    def __init__(self, x):
-        self.__x = x
-    @property
-    def x(self):
-        return self.__x
-    @x.setter
-    def x(self, value):
-        if value < 0:
-            self.__x = 0
-        elif value > 1000:
-            self.__x = 1000
-        else:
-            self.__x = value
-p = P(-1001)
-print(p.x)
+
+def square_matrix_simple(matrix=[]):
+    """
+    Compute the square value of all integers of a matrix
+    """
+    return ([[(x**2) for x in row] for row in matrix])
