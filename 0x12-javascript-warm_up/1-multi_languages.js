9b82c6e7ec73a7b78a2579956a9940f181b51ffa
ANAGONOU Armand <info.armanagonou.com>
2023-03-14 14:50:28 +0100

    fix syntax console.log()

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 const firstLang = 'C is fun';
 const secondLang = 'Python is cool';
 const thirdLang = 'JavaScript is amazing';
-console.log('', firstLang, '\n', secondLang, '\n', thirdLang);
+console.log(firstLang);
+console.log(secondLang);
+console.log(thirdLang);
