346837b88f2c82e069e99dd5aa4faeb583acc7b8
ANAGONOU Armand <info.armanagonou.com>
2023-03-15 07:23:59 +0100

    controlled loop

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const process = require('process');
+
+if (!process.argv[2] || isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  let x = parseInt(process.argv[2]);
+  while (x > 0) {
+    console.log('C is fun');
+    x--;
+  }
+}
