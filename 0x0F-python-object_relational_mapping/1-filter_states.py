494c3f513941de0d43eb0bde2a5219ae958453bf
ANAGONOU Armand <info.armanagonou.com>
2023-03-16 13:32:41 +0100

    filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ args = sys.argv[1:]
 
 conn = MySQLdb.connect(user=args[0], passwd=args[1], db=args[2], charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC ")
+cur.execute("SELECT * FROM states ORDER BY id ASC WHERE name LIKE 'A%'")
 states = cur.fetchall()
 for state in states:
     print(state)
