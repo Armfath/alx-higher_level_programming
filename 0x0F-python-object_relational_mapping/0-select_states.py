9afc25b08a0486cb42c4305aba8d7e97ff98ab15
ANAGONOU Armand <info.armanagonou.com>
2023-03-16 13:06:24 +0100

    first attempt

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module to use MySQLdb
+"""
+import sys
+import MySQLdb
+
+args = sys.argv[1:]
+
+conn = MySQLdb.connect(user=args[0], passwd=args[1], db=args[2], charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+states = cur.fetchall()
+for state in states:
+    print(state)
+cur.close()
+conn.close()
+
