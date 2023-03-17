e760d1fa3a5bdd97148c39ddc719e57d636b82af
ANAGONOU Armand <info.armanagonou.com>
2023-03-17 08:55:26 +0100

    continue performing my_filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     conn = MySQLdb.connect(user=args[0], passwd=args[1], db=args[2])
     cur = conn.cursor()
-    slt = "SELECT * FROM states "
-    cur.execute("%s WHERE name = BINARY '{}' ORDER BY id ASC".format(args[3]) % (slt))
+    slt = "SELECT * FROM states WHERE name "
+    cur.execute("%s = BINARY '{}' ORDER BY id ASC".format(args[3]) % (slt))
     states = cur.fetchall()
     for state in states:
         print(state)
