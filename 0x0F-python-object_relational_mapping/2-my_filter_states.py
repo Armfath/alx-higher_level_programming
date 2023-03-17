1271a878f37d452ec5f1aaf1d607efa2e7475886
ANAGONOU Armand <info.armanagonou.com>
2023-03-17 07:22:06 +0100

    fix code excuting when importing for all

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,14 +5,15 @@ This module to use MySQLdb
 import sys
 import MySQLdb
 
-args = sys.argv[1:]
 
-conn = MySQLdb.connect(user=args[0], passwd=args[1], db=args[2])
-cur = conn.cursor()
-select = "SELECT * FROM states"
-cur.execute(" %s WHERE name = '%s' ORDER BY id ASC" % (select, args[3]))
-states = cur.fetchall()
-for state in states:
-    print(state)
-cur.close()
-conn.close()
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn = MySQLdb.connect(user=args[0], passwd=args[1], db=args[2])
+    cur = conn.cursor()
+    select = "SELECT * FROM states"
+    cur.execute(" %s WHERE name = '%s' ORDER BY id ASC" % (select, args[3]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
