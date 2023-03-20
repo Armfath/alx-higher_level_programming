d6a843febcd9d1ff1346470bf366c15ed2f762c2
Armand ANAGONOU <info.armanagonou@gmail.com>
2023-03-20 08:07:34 -0700

    Query using sqlachemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """Start link class to table in database 
 """
+
+
 import sys
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-            Base.metadata.create_all(engine)
+    # Database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Create the database schema
+    Base.metadata.create_all(engine)
+    # Create a sessionmaker to interact with the database
+    Session = sessionmaker(bind=engine)
+    # Open a session
+    session = Session()
+    # Query States
+    states = session.query(State).order_by(State.id)
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
