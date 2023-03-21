bbd427b2efc26115ae6f0fafe0d20d57dddc01ba
Armand ANAGONOU <info.armanagonou@gmail.com>
2023-03-21 03:51:52 -0700

    pycode fix

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    # Database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Create the database schema
+    Base.metadata.create_all(engine)
+    # Create a sessionmaker to interact with the database
+    Session = sessionmaker(bind=engine)
+    # Open a session
+    session = Session()
+    # Query States
+    states = session.query(State)\
+        .filter(State.name.contains('a'))\
+        .order_by(State.id)
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
