d9767287b76823428d8cb5c4ba951f718dabd559
Armand ANAGONOU <info.armanagonou@gmail.com>
2023-03-24 09:02:13 -0700

    delete record

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+
+
+import sys
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    # Database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    # Create the database schema
+    Base.metadata.create_all(engine)
+    # Create a sessionmaker to interact with the database
+    Session = sessionmaker(bind=engine)
+    # Open a session
+    session = Session()
+    # Query States containning letter 'a' and delete them
+    states = session.query(State)\
+        .filter(State.name.like('%a%'))
+    # display query
+    for state in states:
+        session.delete(state)
+    # commit changes
+    session.commit()
+    # close session to sve ressources
+    session.close()
