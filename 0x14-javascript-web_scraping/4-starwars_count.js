66e4d70a8ead67193df1220e2553f5782b096390
Armfath <info.armanagonou@gmail.com>
2023-04-26 16:42:18 +0100

    higher filter in json like file:

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const process = require('process');
 const request = require('request');
 
 const url = process.argv[2];
-const wedgeAntilles = 'https://swapi-api.alx-tools.com/api/people/1/';
+const wedgeAntilles = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request.get(url, (err, res, body) => {
   if (err) {
